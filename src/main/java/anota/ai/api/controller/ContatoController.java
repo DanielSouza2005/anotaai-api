d0d6cec6d27b465448cc7bbc1bb598cb74d0ab3e
DanielSouza2005 <danisimoes741@gmail.com>
2025-04-13 18:13:56 -0300

    📚 Implemented the SpringDocs for Documentation

--- src/main/java/anota/ai/api/controller/ContatoController.java
+++ src/main/java/anota/ai/api/controller/ContatoController.java
@@ -1,6 +1,7 @@
 package anota.ai.api.controller;
 
 import anota.ai.api.domain.contato.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("contato")
+@SecurityRequirement(name = "bearer-key")
 public class ContatoController {
 
     @Autowired
--- src/main/java/anota/ai/api/controller/UsuarioController.java
+++ src/main/java/anota/ai/api/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package anota.ai.api.controller;
 
 import anota.ai.api.domain.usuario.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/usuario")
+@SecurityRequirement(name = "bearer-key")
 public class UsuarioController {
 
     @Autowired
